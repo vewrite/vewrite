be0788a8ae8a4c643eaec02cb8f7a460ed942100
Rami James <ramijames@gmail.com>
2024-08-08 11:25:42 -0400

    Updated project create form to allow you to select a client from the existing clients

--- pages/projects/create.vue
+++ pages/projects/create.vue
@@ -4,31 +4,52 @@
       <div class="app-panel-header">Create a new project</div>
     </template>
     <template v-slot:body>
-      <form class="inner-container" @submit.prevent="createProject">
-        <div class="form-details">
-            <h3>Project Details</h3>
-            <p class="details">Your project's details.</p>
-        </div>
-        <div class="form-content">
-          <div class="form-group">
-              <div>
-                  <label for="name">Name</label>
-                  <input v-model="project.name" id="name" type="text" />
-              </div>
-              <div>
-                  <label for="status">Status</label>
-                  <input v-model="project.status" id="status" type="text" />
-              </div>
-              
+      <Loading v-if="loading" />
 
-              <div class="form-group-footer">
-                <button type="submit" class="button block primary" :disabled="loading">
-                    <span v-if="loading">Updating...</span>
-                    <span v-else>Create</span>
-                </button>
-              </div>
-          </div>     
+      <!-- {{ clients }} -->
+
+      <form class="inner-container" @submit.prevent="createProject">
+
+        <div class="form-block">
+            <div class="form-details">
+                <h3>Project Details</h3>
+              <p class="details">Your project's details.</p>
+            </div>
+            <div class="form-content">
+              <div class="form-group">
+                <div>
+                    <label for="name">Name</label>
+                    <input v-model="project.name" id="name" type="text" />
+                </div>
+                <div>
+                    <label for="status">Status</label>
+                    <!-- <input v-model="project.status" id="status" type="text" /> -->
+                    <select v-model="project.status" id="status">
+                      <option value="0">Not started</option>
+                      <option value="1">Active</option>
+                      <option value="2">Archived</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label for="client">Client</label>
+                    <select v-model="project.client" id="client">
+                      <option v-for="client in clients" :value="client.id">{{ client.name }}</option>
+                    </select>
+                    
+                </div>
+                
+
+                <div class="form-group-footer">
+                  <button type="submit" class="button block primary" :disabled="loading">
+                      <span v-if="loading">Updating...</span>
+                      <span v-else>Create</span>
+                  </button>
+                </div>
+              </div>     
+            </div>
         </div>
+        
       </form>
     </template>
   </AppPanel>
@@ -37,6 +58,7 @@
 <script setup>
 
 import { useRouter } from 'vue-router';
+import { onMounted } from 'vue'
 
 const router = useRouter();
 const supabase = useSupabaseClient()
@@ -57,9 +79,32 @@ const project = reactive({
                 }
 })
 
+// Get the list of clients
+const clients = ref([])
+
+async function fetchClients() {
+
+  loading.value = true
+
+  const { data, error } = await supabase
+    .from('clients')
+    .select('*')
+    .eq('created_by', user.value.id)
+
+  if (error) {
+    console.error('Error fetching clients:', error.message)
+    return
+  }
+
+  clients.value = data
+
+  loading.value = false
+}
+
 async function createProject() {
   try {
     loading.value = true
+
     const updates = {
       name: project.name,
       status: project.status,
@@ -86,6 +131,11 @@ async function createProject() {
     loading.value = false
   }
 }
+
+onMounted(() => {
+  fetchClients()
+})
+
 </script>
 
 <style scoped>
