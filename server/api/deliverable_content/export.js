780a9659d34f254201fe66e4dc7c4ec05ee8160f
Rami James <ramijames@gmail.com>
2024-12-22 09:29:28 -0500

    HTML export working

--- server/api/deliverable_content/export.js
+++ server/api/deliverable_content/export.js
@@ -1,8 +1,13 @@
 import { defineEventHandler, getQuery, send } from 'h3';
+import { createClient } from '@supabase/supabase-js';
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
   const { id } = query;
+
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
 
   if (!id) {
     throw createError({
@@ -11,7 +16,41 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const fileContent = `${id}`;
+  const { data, error } = await supabase
+    .from('deliverable_content')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'God dammit, error fetching deliverable content',
+    });
+  }
+
+  // const { data, error } = await $fetch('/api/deliverable_content/fetchSingle?id=' + id);
+  // const { data, error } = await $fetch('/api/deliverable_content/fetchSingle', {
+  //   method: 'POST',
+  //   body: JSON.stringify({ id }),
+  // });
+
+  // console.log(data);
+  // console.log(error);
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'God dammit, error fetching deliverable content',
+    });
+  }
+
+  // Ensure fileContent is a string
+  let fileContent = typeof data.content.content === 'string' ? data.content.content : JSON.stringify(data.content.content);
+
+  // Remove double quotes from the beginning and end of the content string
+  fileContent = fileContent.replace(/^"|"$/g, '');
+
   const fileName = `vewrite-export-${id}.md`;
 
   // Set the response headers
---
+++ server/api/deliverable_content/fetchSingle.js
@@ -0,0 +1,38 @@
+import { defineEventHandler, getQuery, send } from 'h3';
+import { createClient } from '@supabase/supabase-js';
+
+export default defineEventHandler(async (event) => {
+  
+  // Initialize Supabase client
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  
+  console.log(supabase);
+
+  const query = getQuery(event);
+  const { id } = query;
+
+  const { data, error } = await supabase
+    .from('deliverable_content')
+    .select('*')
+    .eq('id', id)
+    .single();
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Oof, error fetching deliverable content',
+    });
+  }
+
+  if (!data) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Deliverable content not found',
+    });
+  }
+
+  return send(event, data);
+
+});
