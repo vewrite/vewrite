0c108d9bda2c3807f69ecab43a4e21801fe4ecce
Rami James <ramijames@gmail.com>
2025-02-02 14:11:08 +0200

    Building out structure of TeamAssignment component

--- components/Deliverables/TeamAssignment.vue
+++ components/Deliverables/TeamAssignment.vue
@@ -1,9 +1,47 @@
 <template>
   <section class="team-assignment">
-    {{team}}
+    <div class="members">
+      <div class="members-image" v-for="member in TeamMembersData" :key="member.id">
+        <Avatar :uuid="member.user_id" :hasName="true" size="small" />
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
-  const props = defineProps(['team']);
+
+const props = defineProps(['team']);
+
+import useTeamMembers from '~/composables/useTeamMembers';
+const { fetchTeamMembers, TeamMembersData } = useTeamMembers();
+
+onMounted(async () => {
+  try {
+    await fetchTeamMembers(props.team);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 </script>
+
+<style scoped lang="scss">
+
+@use 'assets/variables' as *;
+
+.team-assignment {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: $border;
+  border-radius: $br-md;
+  padding: $spacing-sm;
+
+  .members {
+    display: flex;
+    flex-direction: column;
+    gap: $spacing-xs;
+  }
+}
+
+</style>
