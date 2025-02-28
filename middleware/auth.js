83f3706b2978d21cff9e1e5f3aa7fd1fa57c5e37
Rami James <ramijames@gmail.com>
2025-02-28 17:38:59 +0200

    Making PlanStatus reactive

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,8 +10,20 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const subscriptionStatus = useState('subscriptionStatus', () => ({}));
   const personaState = useState('personaState', () => ({}));
 
-  // Set subscription status and persona state for use in the app
-  if (user.value) {
+
+  async function handleProfileInserts(payload) {
+    const { new: profile } = payload
+    subscriptionStatus.value = profile.subscription
+    personaState.value = profile.persona
+  }
+
+  async function handleProfileUpdates(payload) {
+    const { new: profile } = payload
+    subscriptionStatus.value = profile.subscription.status
+    personaState.value = profile.persona
+  }
+
+  async function initProfile() {
     const { data, error } = await supabase
       .from('profiles')
       .select('*')
@@ -24,6 +36,29 @@ export default defineNuxtRouteMiddleware(async (to) => {
       subscriptionStatus.value = data.subscription
       personaState.value = data.persona
     }
+  }
+
+  // Set subscription status and persona state for use in the app
+  if (user.value) {
+    // subscription
+    const subscription = supabase
+      .channel('profiles')
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'profiles' }, handleProfileInserts)
+      .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'profiles' }, handleProfileUpdates)
+      .subscribe();
+
+    initProfile()
+
+    onUnmounted(() => {
+      supabase.removeChannel(subscription);
+    });
+
+    // if (error) {
+    //   console.error('Error fetching user profile:', error.message)
+    // } else {
+    //   subscriptionStatus.value = data.subscription
+    //   personaState.value = data.persona
+    // }
   }
 
   // Define public routes
