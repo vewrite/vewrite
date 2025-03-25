bd33f7bc278fd98d1be66c9aba396832c76b3280
Rami James <ramijames@gmail.com>
2025-03-25 10:28:16 +0200

    Sidebar style updates

--- components/TopBar/TopBar.vue
+++ components/TopBar/TopBar.vue
@@ -49,7 +49,6 @@
         <div class="link-group">
           <router-link @click="toggleMobile" to="/projects" class="link"><Icon name="fluent:folder-16-regular" size="1.5rem" /> Projects</router-link>
           <!-- <router-link @click="toggleMobile" to="/workflows" class="link"><Icon name="fluent:flow-16-regular" size="1.5rem" /> Workflows</router-link> -->
-          <!-- <router-link v-if="personaState == 'manager'" @click="toggleMobile" to="/teams" class="link"><Icon name="fluent:people-16-regular" size="1.5rem" /> Teams</router-link> -->
           <router-link v-if="personaState == 'manager'" @click="toggleMobile" to="/clients" class="link"><Icon name="fluent:star-16-regular" size="1.5rem" /> Clients</router-link>
         </div>
       </div>
